77d3f12c88fb226ecf319c3c36146b3c94560c69
Ahsan Iqbal <ahsaniqbal426@gmail.com>
2021-09-27 08:37:53 +0500

    updated

--- src/app/shared/Navbar.js
+++ src/app/shared/Navbar.js
@@ -12,15 +12,17 @@ class Navbar extends Component {
     return (
       <nav className="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
         <div className="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-         <Link className="navbar-brand brand-logo" to="/"><img src={require('../../assets/images/panCakeLogo.JPG')} alt="logo" /></Link>
-         <Link className="navbar-brand brand-logo-mini" to="/"><img src={require('../../assets/images/logo-small.JPG')} alt="logo" /></Link>
+         <Link className="navbar-brand brand-logo" to="/"><img src={require('../../assets/images/Logo.png')} alt="logo" /></Link>
+         <Link className="navbar-brand brand-logo-mini" to="/"><img src={require('../../assets/images/LogoSm.png')} alt="logo" /></Link>
         </div>
         <div className="navbar-menu-wrapper d-flex align-items-stretch">
           <button className="navbar-toggler navbar-toggler align-self-center navbar-toggler-large" type="button" onClick={ () => document.body.classList.toggle('sidebar-icon-only') }>
             <span className="mdi mdi-menu"></span>
           </button>
           <div className="navbar-nav navbar-nav-right">
-            <img src={settingIcon} alt="setting" style={{width:"17px"}}></img>
+            <a href="#">
+              <img src={settingIcon} alt="setting" style={{width:"17px"}}></img>
+            </a>
             <button className="btn poolConnect-WalletBtn ml-3">Connect Wallet</button>
 
           </div>
